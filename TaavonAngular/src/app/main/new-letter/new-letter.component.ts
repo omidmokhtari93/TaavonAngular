a60e5fcb846c4f9e97b2e07a8dee1c6ef2c23907
Omid <Omid@DESKTOP-LJM88T4>
2019-05-20 00:03:32 +0430

    new letter sowe changes

--- TaavonAngular/src/app/main/new-letter/new-letter.component.ts
+++ TaavonAngular/src/app/main/new-letter/new-letter.component.ts
@@ -7,10 +7,21 @@ import * as moment from 'jalali-moment';
   styleUrls: ['./new-letter.component.css']
 })
 export class NewLetterComponent implements OnInit {
-  selectedDate = moment().locale('fa');
+  datePickerConfig = {
+    format:'jYYYY/jMM/jDD'
+  }
+  receivers: string[] = ['شرکت شماره 1', 'شرکت شماره 2'];
+  selectedDate = moment().locale('fa').format('jYYYY/jMM/jDD');
+  hasfile: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  removeBadge(event) {
+    for (var i = this.receivers.length - 1; i >= 0; i--) {
+      if (this.receivers[i] === event.target.innerText) {
+        this.receivers.splice(i, 1);
+      }
+    }
+  }
 }
